2aec099a012b6a4bef65a542d4b29e3fd7988607
WlanKasper <asu.artur.smirnov@gmail.com>
2022-05-07 18:25:21 +0200

    new psw verification

--- util/cli.js
+++ util/cli.js
@@ -1,136 +1,124 @@
 import arg from 'arg';
 import inquirer from 'inquirer';
+const crypto = require("crypto");
 
 const cipher = require('../src/cipher');
 const data = require('../src/manager');
 
-function parseArgumentsIntoOptions() {
-    const args = arg({
-        '--key': String,
+// function parseArgumentsIntoOptions() {
+//     const args = arg({
+//         '--key': String,
 
-        '--git': Boolean,
-        '--yes': Boolean,
-        '--install': Boolean,
+//         '--git': Boolean,
+//         '--yes': Boolean,
+//         '--install': Boolean,
 
-        '-k': '--key',
+//         '-k': '--key',
 
-        '-g': '--git',
-        '-y': '--yes',
-        '-i': '--install'
+//         '-g': '--git',
+//         '-y': '--yes',
+//         '-i': '--install'
+//     });
+
+//     return {
+//         action: true,
+//         aeskey: true,
+//     };
+// }
+
+async function menu(authorization) {
+    // MENU
+    const menuAnswers = await inquirer.prompt({
+        type: 'list',
+        name: 'action',
+        message: 'Menu',
+        choices: ['<-> Add new account', '<-> Find account', '<-> Delete all', '<-> Exit'],
     });
 
-    return {
-        action: true,
-        aeskey: true,
-    };
-}
+    let jsonTempData = data.initJSON('WlanKasper',authorization);
 
-async function menu() {
-    if (cipher.checkKeys()) {
-        // AES KEY PASSWORD
-        let firsQuestion = {
-            type: 'password',
-            name: 'aeskey',
-            message: 'Enter your AES password: ',
-            mask: '*'
-        };
+    switch (menuAnswers.action) {
 
-        const firstAnswers = await inquirer.prompt(firsQuestion);
+        case '<-> Add new account':
+            let questions = [];
 
-        var jsonTempData = data.requireDataFromFile(firstAnswers.aeskey);
+            questions.push({
+                type: 'input',
+                name: 'company',
+                message: 'Enter company name: '
+            });
 
-        if (jsonTempData === 'err_psw'){
-            console.log('\nPassword Error\n');
-        }else{
-            if (jsonTempData === 'err_file'){
-                jsonTempData = data.initJSON('Artur', jsonTempData);
-                data.createDir();
-                data.saveDataToFile(jsonTempData);
-            }
-            // MENU
-            let secondQuestion = {
-                type: 'list',
-                name: 'action',
-                message: 'Menu',
-                choices: ['<-> Add new account', '<-> Find account', '<-> Delete all', '<-> Exit'],
+            questions.push({
+                type: 'input',
+                name: 'login',
+                message: 'Enter login: '
+            });
+
+            questions.push({
+                type: 'password',
+                name: 'password',
+                message: 'Enter password: ',
+                mask: '*'
+            });
+
+            const answers = await inquirer.prompt(questions);
+            jsonTempData = data.addCollectionToJSON(jsonTempData, answers.company, answers.login, answers.password);
+            data.saveDataToFile(jsonTempData);
+            break;
+
+        case '<-> Find account':
+            const companyAnswer = await inquirer.prompt({
+                type: 'input',
+                name: 'company',
+                message: 'Enter company name: '
+            });
+
+            console.log(data.getCollectionByCompany(jsonTempData, companyAnswer.company));
+            break;
+
+        case '<-> Delete all':
+            let realy = {
+                type: 'confirm',
+                name: 'deleteAll',
+                message: 'You will never be able to recover the data.\nAre you sure you want to delete all the data?: ',
+                default: false
             };
-            const secondAnswers = await inquirer.prompt(secondQuestion);
-
-            switch (secondAnswers.action) {
-
-                case '<-> Add new account':
-                    let questions = [];
-
-                    questions.push({
-                        type: 'input',
-                        name: 'company',
-                        message: 'Enter company name: '
-                    });
-
-                    questions.push({
-                        type: 'input',
-                        name: 'login',
-                        message: 'Enter login: '
-                    });
-
-                    questions.push({
-                        type: 'password',
-                        name: 'password',
-                        message: 'Enter password: ',
-                        mask: '*'
-                    });
-
-                    const answers = await inquirer.prompt(questions);
-
-                    jsonTempData = data.addCollectionToJSON(jsonTempData, answers.company, answers.login, answers.password);
-                    data.saveDataToFile(jsonTempData);
-                    break;
-
-                case '<-> Find account':
-                    let company = {
-                        type: 'input',
-                        name: 'company',
-                        message: 'Enter company name: '
-                    };
-
-                    const answerCompany = await inquirer.prompt(company);
-
-                    console.log(data.getCollectionByCompany(jsonTempData, answerCompany.company));
-                    break;
-
-                case '<-> Delete all':
-                    let realy = {
-                        type: 'confirm',
-                        name: 'deleteAll',
-                        message: 'You will never be able to recover the data.\nAre you sure you want to delete all the data?: ',
-                        default: false
-                    };
-                    const answerToDelete = await inquirer.prompt(realy);
-                    if (answerToDelete.deleteAll == true){
-                        data.deleteFile();
-                    }
-                    break;
-
-                case '<-> Exit':
-                    return true;
+            const answerToDelete = await inquirer.prompt(realy);
+            if (answerToDelete.deleteAll == true) {
+                data.deleteFile();
             }
-        }
-    } else {
-        // AES KEY PASSWORD
-        let firsQuestion = {
-            type: 'password',
-            name: 'rsakey',
-            message: 'Welcome!\nYou have to create AES key to encript private RSA key\nEnter your password: ',
-            mask: '*'
-        };
+            break;
 
-        const firstAnswers = await inquirer.prompt(firsQuestion);
-
-        cipher.createKeyPair(firstAnswers.rsakey);
+        case '<-> Exit':
+            return true;
     }
 }
 
 export async function cli() {
     // let options = parseArgumentsIntoOptions();
-    await menu();
+
+    if (cipher.checkKeys()) {
+        const passwordAnswer = await inquirer.prompt({
+            type: 'password',
+            name: 'aeskey',
+            message: 'Enter your AES password: ',
+            mask: '*'
+        });
+
+        if (cipher.checkPsw(passwordAnswer.aeskey)) {
+            await menu(passwordAnswer.aeskey);
+        } else {
+            await cli();
+        }
+    } else {
+        const passwordAnswer = await inquirer.prompt({
+            type: 'password',
+            name: 'rsakey',
+            message: 'Welcome!\nYou have to create AES key to encript private RSA key\nEnter your password: ',
+            mask: '*'
+        });
+
+        cipher.createKeyPair(passwordAnswer.rsakey);
+        await menu(passwordAnswer.aeskey);
+    }
 }
